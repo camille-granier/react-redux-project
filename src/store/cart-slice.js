4b7b91be208dcb86204d86c2672d303e7e132410
camille-granier <granierc@protonmail.com>
2021-11-29 15:05:16 +0100

    adding button functions and cart update

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,34 +1,42 @@
-import {createSlice} from '@reduxjs/toolkit';
-
-const initialCartState = {};
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
-  } ,
+  },
   reducers: {
     addItemToCart(state, action) {
-      const newItem = action.playload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
-        //reminder: manipulating state ok because we are using redux toolkit, otherwise never do it
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title
         });
       } else {
-        //reduxtoolkit ok to manipulate state
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
-      const id = action.playload;
-    }
-  }
-})
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
