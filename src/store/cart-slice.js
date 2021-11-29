53ab6de48812bfc16d713b75235e6c6de1a2e5d2
camille-granier <granierc@protonmail.com>
2021-11-29 14:06:32 +0100

    modification cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,8 +10,25 @@ const cartSlice = createSlice({
   } ,
   reducers: {
     addItemToCart(state, action) {
-      item = action.playload
+      const newItem = action.playload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      if (!existingItem) {
+        //reminder: manipulating state ok because we are using redux toolkit, otherwise never do it
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        //reduxtoolkit ok to manipulate state
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
     },
-    removeItemFromCart(state, action) {}
+    removeItemFromCart(state, action) {
+      const id = action.playload;
+    }
   }
 })
